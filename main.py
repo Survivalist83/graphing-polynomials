78e2632e5a74e9c027d997f9c6b782595916969c
Survivalist83 <benmaxtennant@gmail.com>
2023-11-23 10:37:05 -0800

    supports up to three side-by-side graphs

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def write_polynomial(polynomial):
             poly = poly + str(abs(polynomial[i]) if abs(polynomial[i]) != 1 else '') + 'x^' + str(len(polynomial) - i - 1)
     return poly
 
-def calculate_y(polynomial, x):
+def calculate_y(polynomial, x, degree):
     y_val = 0
     for j in range(degree + 1):
         y_val = y_val + (x ** (degree - j) * polynomial[j])
@@ -23,59 +23,65 @@ def calculate_y(polynomial, x):
 
 # Window settings
 with st.sidebar:
-    col1, col2 = st.columns(2)
 
-    with col1: # Polynomial Tab; sets the polynomial to be drawn by the program
-        st.header('Polynomial')
-        degree = st.number_input(label='Select the degree of the polynomial:', value=3, min_value=1, step=1)
-        polynomial = []
-        for i in range(degree + 1):
-            polynomial.append(st.number_input(f"Enter coefficient for x^{degree - i}", key=f"coeff_{i}", value=1.0, step=1.0))
-        st.write('Polynomial Shown: ' + write_polynomial(polynomial))
+    # Window tab; sets the desired window for the drawing
+    st.header('Window')
+    X_MIN = st.number_input(label='Left X Bound', value=-10.0, step=0.1)
+    X_MAX = st.number_input(label='Right X Bound', value=10.0, step=0.1)
+    X_STEP = st.number_input(label='X Step', value=1.0, step=0.1)
+    I_MIN = st.number_input(label='Left I Bound', value=-10.0, step=0.1)
+    I_MAX = st.number_input(label='Right I Bound', value=10.0, step=0.1)
+    I_STEP = st.number_input(label='I Step', value=1.0, step=0.1)
+    DOT_SIZE = st.number_input(label='Dot Size', value=3, min_value=1, max_value=10, step=1)
+    GRAPH_COUNT = st.number_input(label='Number of Graphs', value=1, min_value=1, max_value=3)
 
-    with col2: # Window tab; sets the desired window for the drawing
-        st.header('Window')
-        X_MIN = st.number_input(label='Left X Bound', value=-10.0, step=0.1)
-        X_MAX = st.number_input(label='Right X Bound', value=10.0, step=0.1)
-        X_STEP = st.number_input(label='X Step', value=1.0, step=0.1)
-        I_MIN = st.number_input(label='Left I Bound', value=-10.0, step=0.1)
-        I_MAX = st.number_input(label='Right I Bound', value=10.0, step=0.1)
-        I_STEP = st.number_input(label='I Step', value=1.0, step=0.1)
-        DOT_SIZE = st.number_input(label='Dot Size', value=3, min_value=1, max_value=10, step=1)
+def graph_polynomial(column):
+    # Polynomial Tab; sets the polynomial to be drawn by the program
+    st.header(f'Polynomial {column + 1}' if GRAPH_COUNT != 1 else 'Polynomial')
+    degree = st.number_input(label='Select the degree of the polynomial:', key=f'degree{column}', value=3, min_value=1, step=1)
+    polynomial = []
+    for i in range(degree + 1):
+        polynomial.append(st.number_input(f'Enter coefficient for x^{degree - i}', key=f'coeff_{i}_{column}', value=1.0, step=1.0))
+    st.write('Polynomial Shown: ' + write_polynomial(polynomial))
 
-real = st.checkbox(label='Show real part of y', value=True)
-imag = st.checkbox(label='Show imaginary part of y when y is complex', value=False)
+    real_imag = st.radio(label='What do you want to show?', options=['Real', 'Imaginary', 'Complex'], key=f'radio{column}', index=column)
+    real = real_imag == 'Real' or real_imag == 'Complex'
+    imag = real_imag == 'Imaginary' or real_imag == 'Complex'
 
-# calculates dot locations
-points = pd.DataFrame(columns=['x', 'i', 'y', 'type'])
-for x in np.arange(X_MIN, X_MAX + 1, X_STEP):
-    for i in np.arange(I_MIN, I_MAX + 1, I_STEP):
-        if (real): points.loc[len(points)] = {'x': x, 'i': i, 'y': calculate_y(polynomial, complex(x, i)).real, 'type': 'real'}
-        if (imag): points.loc[len(points)] = {'x': x, 'i': i, 'y': calculate_y(polynomial, complex(x, i)).imag, 'type': 'imag'}
+    # calculates dot locations
+    points = pd.DataFrame(columns=['x', 'i', 'y', 'type'])
+    for x in np.arange(X_MIN, X_MAX + 1, X_STEP):
+        for i in np.arange(I_MIN, I_MAX + 1, I_STEP):
+            if (real): points.loc[len(points)] = {'x': x, 'i': i, 'y': calculate_y(polynomial, complex(x, i), degree).real, 'type': 'real'}
+            if (imag): points.loc[len(points)] = {'x': x, 'i': i, 'y': calculate_y(polynomial, complex(x, i), degree).imag, 'type': 'imag'}
 
-# dot colors
-points.loc[(points['type'] == 'real'), 'color'] = '#FF4031'
-points.loc[(points['type'] == 'imag'), 'color'] = '#0541FF'
-points.loc[(points['type'] == 'real') & (points['y'] == 0), 'color'] = '#9E0000'
-points.loc[(points['type'] == 'imag') & (points['y'] == 0), 'color'] = '#00009E'
-points.loc[(points['type'] == 'real') & (points['i'] == 0), 'color'] = '#85FF7D'
-points.loc[(points['type'] == 'imag') & (points['i'] == 0), 'color'] = '#00FF00'
+    # dot colors
+    points.loc[(points['type'] == 'real'), 'color'] = '#FF4031'
+    points.loc[(points['type'] == 'imag'), 'color'] = '#0541FF'
+    points.loc[(points['type'] == 'real') & (points['y'] == 0), 'color'] = '#9E0000'
+    points.loc[(points['type'] == 'imag') & (points['y'] == 0), 'color'] = '#00009E'
+    points.loc[(points['type'] == 'real') & (points['i'] == 0), 'color'] = '#85FF7D'
+    points.loc[(points['type'] == 'imag') & (points['i'] == 0), 'color'] = '#00FF00'
 
-# graph
+    # graph
+    fig = go.Figure(data=[go.Scatter3d(x=points['x'], y=points['i'], z=points['y'], mode='markers',
+                                    marker=dict(color=points['color'], size=DOT_SIZE))])
 
-fig = go.Figure(data=[go.Scatter3d(x=points['x'], y=points['i'], z=points['y'], mode='markers',
-                                  marker=dict(color=points['color'], size=DOT_SIZE))])
+    fig.update_layout(scene=dict(aspectratio=dict(x=1, y=1, z=1), 
+                                xaxis=dict(title='Real'),
+                                yaxis=dict(title='Imaginary'),
+                                zaxis=dict(title='Y Real')))
 
-fig.update_layout(scene=dict(aspectratio=dict(x=1, y=1, z=1), 
-                             xaxis=dict(title='Real'),
-                             yaxis=dict(title='Imaginary'),
-                             zaxis=dict(title='Y Real')))
+    st.plotly_chart(fig, use_container_width=True, sharing='streamlit')
 
-st.plotly_chart(fig, use_container_width=True, sharing='streamlit')
+columns = st.columns(GRAPH_COUNT)
+for i, col in enumerate(columns):
+    with col:
+        graph_polynomial(i)
 
 # explains what each color is
 st.header('Explanation of Colors')
-st.write('Red represents the real part of y, and blue is the imaginary part. You can show/hide them at your convenience with the checkboxes above.')
+st.write('Red represents the real part of y, and blue is the imaginary part. You can show/hide them at your convenience with the radio buttons above.')
 st.write('Green is when i is zero, aka what you see when you plug it into your graphing calculator.')
 st.write('When y is equal to zero, the red/blue is a bit darker. Green stays the same when y equals zero.')
 
